e3ae4c01094269b00f454fa1dba920a6d80b367b
krishnamadugu <krishnamusalimadugu@gmail.com>
2025-02-09 00:46:57 +0530

    feat : calendar functionality - cubit - implemented

--- lib/feat/add_emp_details/view_model/add_emp_cubit.dart
+++ lib/feat/add_emp_details/view_model/add_emp_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:employee_info/core/services/storage_service/hive_storage/app_database.dart';
 import 'package:employee_info/core/utils/common_widgets/toast_msg.dart';
+import 'package:employee_info/feat/add_emp_details/view_model/calendar_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
@@ -35,29 +36,59 @@ class AddEmpCubit extends Cubit<EmployeeModel> {
         ));
 
   /// on add emp btn selected
-  void onAddEmpBtnPressed() {
-    if (joiningDateTime == null) {
+  void onAddEmpBtnPressed(BuildContext context) {
+    if (context.read<CalendarCubit>().fromDate.text.isEmpty) {
       showToastMsg("Please enter from date");
-    } else if (exitedDateTime == null) {
-      showToastMsg("Please enter to date");
     } else if (selectedDesignationVal.isEmpty) {
       showToastMsg("Please choose your designation");
     } else if (empName.text.isEmpty) {
       showToastMsg("Please enter employee name");
     } else {
-      /// date format for adding employee model to list
-      var formatter = DateFormat('d MMM, yyyy');
-      String joiningDate = formatter.format(joiningDateTime!);
-      String exitedDate = formatter.format(exitedDateTime!);
+      String joiningDate = context.read<CalendarCubit>().fromDate.text;
+      String exitedDate = context.read<CalendarCubit>().toDate.text;
 
       var emp = EmployeeModel(
         name: empName.text,
         designation: selectedDesignationVal,
-        fromDate: joiningDate ?? "",
-        toDate: exitedDate ?? "",
+        fromDate: joiningDate,
+        toDate: exitedDate,
       );
 
       AppDatabase.instance.saveEmployee(emp);
+      Navigator.of(context).pop();
     }
+  }
+
+  /// set default values
+  void setDefaultValues() {
+    empName.text = "";
+    selectedDesignationVal = "";
+    var emp = EmployeeModel(
+      name: empName.text,
+      designation: selectedDesignationVal,
+      fromDate: "",
+      toDate: "",
+    );
+    emit(emp);
+  }
+
+  /// on drop down value selected
+  void onDropDownValSelected({required String roleSelected}) {
+    /// date format for adding employee model to list
+
+    var formatter = DateFormat('d MMM, yyyy');
+    String joiningDate =
+        joiningDateTime == null ? "" : formatter.format(joiningDateTime!);
+    String exitedDate =
+        exitedDateTime == null ? "" : formatter.format(exitedDateTime!);
+
+    var emp = EmployeeModel(
+      name: empName.text,
+      designation: selectedDesignationVal,
+      fromDate: joiningDate,
+      toDate: exitedDate,
+    );
+
+    emit(emp);
   }
 }
